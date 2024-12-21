849e848311039b564615e01baea6c5efd5f3f4d8
Ziad Emad <ziaemad0111@gmail.com>
2024-12-21 22:03:35 +0200

    Create Nav, Home, Portfolio, Wedding, Footer

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+let btnContainer = document.getElementById('item-active');
+let btns = document.getElementsByClassName('btn');
+
+for (let i = 0; i < btns.length; i++){
+    btns[i].addEventListener('click', function(){
+        let current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace(' active', '');
+        this.className += " active";
+    });
+}
+
+
