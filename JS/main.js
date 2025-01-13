6202e1ccf145f00368b1cea18c6c953ac40d4821
Ziad Emad <ziaemad0111@gmail.com>
2025-01-13 18:12:18 +0200

    Update Style  , Dark Mode

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+// ? Active item
 let btnContainer = document.getElementById('item-active');
 let btns = document.getElementsByClassName('btn');
 
@@ -8,5 +9,19 @@ for (let i = 0; i < btns.length; i++){
         this.className += " active";
     });
 }
-
-
+// ? Dark mode
+let darkMode = localStorage.getItem('darkMode');
+const changeMode = document.getElementById('changeMode');
+const enableDarkMode = () => {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'active');
+}
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', null);
+}
+if (darkMode === "active") enableDarkMode();
+changeMode.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    darkMode !== "active" ? enableDarkMode() : disableDarkMode();
+});
